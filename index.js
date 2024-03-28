2362050367bff05581670e99999d0f48ce2f23f6
Nicollas dos Santos <nicollasmk9@gmail.com>
2024-03-27 21:26:51 -0300

    Button still not working

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ function resultado(){
     let num2 = Number(document.getElementById("num-two").value);
     let total = 0;
     
-    if(document.getElementById('box1').checked){
+    if(document.getElementById('box1').checked)
         total = num1 + num2;
-    }
-    else if(document.getElementById('box2').checked){
+    else if(document.getElementById('box2').checked)
         total = num1 - num2;
-    }
-    else if(document.getElementById('box3').checked){
+    else if(document.getElementById('box3').checked)
         total = num1 * num2;
-    }
-    else{
+    else
         total = num1 / num2;
-    }
+    
 }
 document.getElementById('resultArea').innerHTML = "Result is: " + String(total);
